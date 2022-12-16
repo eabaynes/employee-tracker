d4b88711f304ddbd708b2a9de9be9552507ac665
Elliot Baynes <sbaynes1994@gmail.com>
2022-12-15 18:18:30 -0600

    add inquirer questsions

--- index.js
+++ index.js
@@ -5,12 +5,62 @@
 // email in HTML can be clicked on and will link to default email app
 // clickable github links where applicable
 
-// manager: name, employee ID, email. office number
-
-// do you want to add an engineer or intern?
-
-// engineer: name, Id, email. github
-
-// intern: name, id, email, school
+// do you want to add a manager, engineer or intern?
 
 // html is generated from all responses
+
+import inquirer from "inquirer"
+
+// general questions for every employee
+const employeeQuestions = [
+    {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role?",
+        choices: ["Manager", "Intern", "Engineer"]
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?"
+    },
+    // todo add validate to check that id is a number
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee's ID number?"
+    },
+    // todo add validate to check that email is valid format
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email?"
+    }
+]
+
+// unique question for the managers
+const managerQuestion= [
+    {
+        type: "input",
+        name: "office",
+        message: "What office is this Manager located at?"
+    }
+]
+
+// unique question for the interns
+const internQuestion= [
+    {
+        type: "input",
+        name: "school",
+        message: "What school is the intern enrolled at?"
+    }
+]
+
+// unique question for the engineers
+const engineerQuestion= [
+    {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's github account?"
+    }
+]
